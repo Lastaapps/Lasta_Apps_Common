7bb81b5da4e555a364720191f15f8e3f30d27197
Lasta apps <krasik.peta@seznam.cz>
2021-06-02 23:33:51 +0200

    Version 1.1.1
    In app Review improved

--- lib/src/main/java/cz/lastaapps/common/PlayStoreReview.kt
+++ lib/src/main/java/cz/lastaapps/common/PlayStoreReview.kt
@@ -1,57 +1,111 @@
+/*
+ *   Copyright 2021, Petr Laštovička as Lasta apps, All rights reserved
+ *
+ *     This file is part of President Countdown.
+ *
+ *     This app is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This app is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this app.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 package cz.lastaapps.common
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
 
 object PlayStoreReview {
 
     private val TAG get() = PlayStoreReview::class.simpleName
 
+    fun askForAppReview(activity: Activity) {
+        showOrLegacy(activity) {
+            with(AlertDialog.Builder(activity)) {
+                setTitle(R.string.review_dialog_title)
+                setMessage(R.string.review_dialog_message)
+                setPositiveButton(R.string.review_dialog_positive) { dialog, _ ->
+                    dialog.dismiss()
+                    openPlayStore(activity)
+                }
+                setNegativeButton(R.string.review_dialog_negative) { dialog, _ ->
+                    dialog.dismiss()
+                }
+                create()
+            }.show()
+        }
+    }
+
     fun doInAppReview(activity: Activity) {
+        showOrLegacy(activity) { openPlayStore(activity) }
+    }
+
+    private fun showOrLegacy(activity: Activity, onLegacy: () -> Unit) {
         val manager = ReviewManagerFactory.create(activity)
 
-        //redirects to the play store, required under LOLLIPOP 5.0 and when Google play API fails
-        val oldRequest: () -> Unit by lazy {
-            {
-                Log.i(TAG, "Launching old Play Store review link")
-
-                val url = "https://play.google.com/store/apps/details?id=${activity.packageName}"
-                val uri = Uri.parse(url)
-                activity.startActivity(Intent(Intent.ACTION_VIEW, uri))
-            }
+        //version check
+        @SuppressLint("ObsoleteSdkInt")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            onLegacy()
+            return
         }
 
-        //version check
-        /*if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            oldRequest()
-            return
-        }*/
+        openInAppReview(
+            manager, activity, onLegacy
+        )
+    }
+
+    private fun openPlayStore(context: Context) {
+        Log.i(TAG, "Launching old Play Store review link")
+
+        val url = "https://play.google.com/store/apps/details?id=${context.packageName}"
+        val uri = Uri.parse(url)
+        context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    private fun openInAppReview(mgr: ReviewManager, activity: Activity, onFail: () -> Unit) {
 
         //Google play in app review
-        val request = manager.requestReviewFlow()
+        val request = mgr.requestReviewFlow()
         request.addOnCompleteListener { result ->
             if (result.isSuccessful) {
 
                 Log.i(TAG, "A review dialog shown")
 
                 val reviewInfo = result.result
-                val flow = manager.launchReviewFlow(activity, reviewInfo)
+                val flow = mgr.launchReviewFlow(activity, reviewInfo)
+
                 flow.addOnCompleteListener {
 
                     Log.i(TAG, "A review dialog closed")
 
-                    Toast.makeText(
-                        activity,
-                        R.string.thanks_review,
-                        Toast.LENGTH_LONG
-                    ).show()
+                    if (it.isSuccessful) {
+                        Toast.makeText(
+                            activity,
+                            R.string.review_thanks,
+                            Toast.LENGTH_LONG,
+                        ).show()
+                    }
                 }
             } else {
-                oldRequest()
+                onFail()
             }
         }
     }
